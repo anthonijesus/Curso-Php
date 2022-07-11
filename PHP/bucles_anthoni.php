5e7919f8e3942cc9c4501a42238d3f666144cbe9
anthonijesus <anthonijesusmail.com>
2022-07-11 20:23:34 -0300

    Actualizacion media clase del 11.07.2022

---
+++ PHP/bucles_anthoni.php
@@ -0,0 +1,31 @@
+<?php
+
+   $i = 0; 
+   while ($i < 5){
+    echo "<h1> $i </h1>";
+    $i ++;
+   } 
+
+   echo "<hr/>";
+
+   $j = 0;
+   $sigo = true;
+
+   while ($sigo){
+    echo "<h1> $j </h1>";
+    $j ++;
+    if($j >= 5){
+       $sigo = false;
+    }
+   }
+    
+   echo "<hr/>";
+   for($k = 0; $k < 5; $k++){
+    echo "<h1> $k </h1>";
+   }
+
+   
+
+
+
+?>
